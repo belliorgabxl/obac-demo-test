2498f27f8e286f4bbff121c361c662fe78d2125f
patarajarin <patara1919@gmail.com>
2024-12-10 02:13:36 +0700

    delete

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -1,5 +1,4 @@
-import { CarouselData } from "@/resource/home/carosuelData";
-import Carousel1 from "@/app/components/carousel/carousel-1";
+
 import { SidebarData } from "@/resource/home/sidebarData";
 import CardHorizontal from "@/app/components/card/card-horizontal";
 import { cardData } from "@/resource/fetchData/cardContent";
